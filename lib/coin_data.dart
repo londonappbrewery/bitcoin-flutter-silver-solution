fd513dbd1a9789fed739d125868728f3d8219dcb
Shahrukh Shikalgar <shaharukhs@gmail.com>
2020-05-04 13:50:29 +0530

    Update coin_data.dart
    update old URL with new API url

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -38,24 +38,27 @@ const apiKey = 'YOUR-API-KEY-HERE';
 
 class CoinData {
   Future getCoinData(String selectedCurrency) async {
-    //4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
-    //5: Return a Map of the results instead of a single value.
-    Map<String, String> cryptoPrices = {};
-    for (String crypto in cryptoList) {
-      //Update the URL to use the crypto symbol from the cryptoList
-      String requestURL =
-          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
-      http.Response response = await http.get(requestURL);
-      if (response.statusCode == 200) {
-        var decodedData = jsonDecode(response.body);
-        double lastPrice = decodedData['rate'];
-        //Create a new key value pair, with the key being the crypto symbol and the value being the lastPrice of that crypto currency.
-        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
-      } else {
-        print(response.statusCode);
-        throw 'Problem with the get request';
-      }
+
+       //3. Create the Asynchronous method getCoinData() that returns a Future (the price data).
+  Future getCoinData() async {
+    //4. Create a url combining the coinAPIURL with the currencies we're interested, BTC to USD.
+    String requestURL = '$coinAPIURL/BTC/$selectedCurrency?apikey=$apiKey';
+    //5. Make a GET request to the URL and wait for the response.
+    http.Response response = await http.get(requestURL);
+
+    //6. Check that the request was successful.
+    if (response.statusCode == 200) {
+      //7. Use the 'dart:convert' package to decode the JSON data that comes back from coinapi.io.
+      var decodedData = jsonDecode(response.body);
+      //8. Get the last price of bitcoin with the key 'last'.
+      var lastPrice = decodedData['rate'];
+      //9. Output the lastPrice from the method.
+      return lastPrice;
+    } else {
+      //10. Handle any errors that occur during the request.
+      print(response.statusCode);
+      //Optional: throw an error if our request fails.
+      throw 'Problem with the get request';
     }
-    return cryptoPrices;
   }
 }
